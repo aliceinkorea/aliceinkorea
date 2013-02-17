1145c1718006630e1841e81014e53f4f361855a2
yeaz <yeaz@stanford.edu>
2013-02-17 14:55:59 +0900

    added carrierwave initializer

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,12 @@
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    :provider               => ENV['FOG_PROVIDER'],                        # required
+    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],                        # required
+    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],                        # required
+    #:host                   => 's3.example.com',             # optional, defaults to nil
+    #:endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
+  }
+  config.fog_directory  = ENV['FOG_DIRECTORY']                    # required
+  #config.fog_public     = false                                   # optional, defaults to true
+ # config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+end
